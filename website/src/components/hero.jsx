197af656bfe951ae1e3940098d4b027db66ccde8
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2026-05-29 22:42:17 -0400

    Create hero section

---
+++ website/src/components/hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return ( 
+        <div className="hero">
+            <span>lorem ipsum</span>
+            <h1>Hello, World!</h1>
+        </div>
+     );
+}
+ 
+export default Hero;

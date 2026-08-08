1d3edcdd328ded19912901fa9907e81fee337ade
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2026-08-07 22:01:10 -0400

    current status of the project (I got lost)

--- website/src/components/HeroSection.jsx
+++ website/src/components/HeroSection.jsx
@@ -1,32 +1,38 @@
 import SolidButton from "./ComponentParts/SolidButton";
-import {move_to_section} from "../functions/Pretty_design_functions";
+import { move_to_section } from "../functions/Pretty_design_functions";
 
 const Hero = () => {
   return (
     <div className="h-screen relaitve z-0" id="header">
       {/* Background Image div */}
-      <div
-        className="bg-cover bg-center bg-[url(assets/header_bg_mid.jpg)] opacity-30 absolute inset-0 z-10"
+      {/* <div
+        className="bg-cover bg-center bg-[url(assets/header_bg_mid.jpg)] opacity-90 absolute inset-0 z-10"
         aria-label="Photo by Christina @ wocintechchat.com M"
         id="header_background_image"
-      />
-      {/* Background gradient positioned above the div */}
-      <div className="bg-linear-to-r from-cyan-100 via-blue-300 to-indigo-400 opacity-40 inset-0 absolute z-20" />
+      /> */}
       {/* Content positioned above the gradient */}
-      <div className="flex flex-col justify-center items-center gap-5 py-5 h-full opacity-100 relative z-30">
-        <span className="space-y-2 text-center w-2/3">
-          <h1 className="text-6xl">Professional and Academic Consulting</h1>
-          <span className="text-gray-700 ">
+      <div className="flex flex-col justify-center gap-5 py-5 px-40 h-full opacity-100 relative z-30">
+        <span className="space-y-2 w-2/3">
+          <h1 className="text-6xl heading-font py-3">
+            Career Guidance,
+            <br />
+            Just for You!
+          </h1>
+          <span className="text-gray-700 content-font text-lg">
             Supporting executives, professionals, researchers, and students in
             building impactful résumés, academic portfolios, graduate
             applications, defense slideshows, and thesis/dissertation materials.
           </span>
         </span>
         <span className="w-fit">
-          <SolidButton children="Get Started" className="shadow-md text-white" onClick={()=>move_to_section("meet_instructor")}/>
+          <SolidButton
+            children="Get Started"
+            className="shadow-md text-white"
+            onClick={() => move_to_section("meet_instructor")}
+          />
         </span>
       </div>
-      <div className="border-b-1 border-gray-400"/>
+      <div className="border-b-1 border-gray-400" />
     </div>
   );
 };
--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import { Briefcase } from "lucide-react";
+import logo from "../assets/logos/nav.svg";
 import OutlineButton from "./ComponentParts/OutlineButton";
 import { move_to_section } from "../functions/Pretty_design_functions";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center fixed py-5 px-40 bg-white w-full z-100 shadow-md">
+    <nav className="flex justify-between items-center fixed px-40 bg-white w-full z-100 shadow-md content-font">
       <div className="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text">
-        <h3 className="text-xl font-serif text-transparent">Career Studio 101</h3>
+        <img src={logo} alt="Logo" className="h-20" />
       </div>
       <div className="flex gap-3 h-fit items-center">
-        <span className="font-semibold text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("meet_instructor")}>Meet your instructor</span>
-        <span className="font-semibold text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("How_it_works")}>How it works</span>
-        <span className="font-semibold text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("packages")}>Packages</span>
-        <span className="font-semibold text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("solo_services")}>Solo services</span>
+        <span className="text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("meet_instructor")}>Meet your instructor</span>
+        <span className="text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("How_it_works")}>How it works</span>
+        <span className="text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("packages")}>Packages</span>
+        <span className="text-slate-600 hover:cursor-pointer" onClick={()=>move_to_section("solo_services")}>Solo services</span>
         {/* takes you to packages */}
-        <OutlineButton children={"Book"} className="text-slate-600 font-semibold shadow-md" onClick={()=>move_to_section("book_a_session")}/>
+        <OutlineButton children={"Book"} className="text-slate-600 shadow-md" onClick={()=>move_to_section("book_a_session")}/>
       </div>
     </nav>
   );
